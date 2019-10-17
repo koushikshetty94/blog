ad3145c2270c75a64b0543763a087f5ff7d0fda4
koushik shetty <koushikshetty94@gmail.com>
2019-10-17 11:41:38 +0530

    second edit

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+export default function Logo(){
+    return(
+        <Link to ="/"><h1 style={{fontSize: '2.5rem' }}>Koushik Shetty</h1></Link>
+    );
+}
